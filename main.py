d594e91acc090afe6a4287521011383d21de4226
Azamat Bayramov <bayramov.azamat04@gmail.com>
2022-12-19 20:28:25 +0300

    Add some features

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import asyncio
+
+import emoji
 from aiogram import Bot, Dispatcher, types
 
 GRADES = {
@@ -10,6 +12,26 @@ GRADES = {
     "F": 2
 }
 
+SCHOLARSHIP_EMOJI = {
+    3: ":dizzy_face:",
+    4: ":astonished:",
+    5: ":confounded:",
+    6: ":sob:",
+    7: ":cold_sweat:",
+    8: ":fearful:",
+    9: ":cry:",
+    10: ":pensive:",
+    11: ":unamused:",
+    12: ":grimacing:",
+    13: ":confused:",
+    14: ":simple_smile:",
+    15: ":relieved:",
+    16: ":relaxed:",
+    17: ":grinning:",
+    18: ":smirk:",
+    19: ":blush:",
+    20: ":heart_eyes:"
+}
 B_min = 3000
 B_max = 20000
 TOKEN = open("token.txt").readline().strip()
@@ -19,7 +41,11 @@ async def parse_grades(grades_str: str) -> list[int]:
     grades = list()
 
     for grade_str in grades_str:
-        grades.append(GRADES[grade_str])
+        if grade_str.isdigit():
+            n = int(grade_str) - 1
+            grades += [grades[-1]] * n
+        else:
+            grades.append(GRADES[grade_str])
 
     return grades
 
@@ -39,8 +65,15 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=["start", "help"])
 async def handle_help(message: types.Message):
-    await message.answer(
-        "Hello! I will help you to calculate your scholarship.\nJust send me your grades in a row. Example: AABBCP\n\nProgrammer: @azamatbayramov")
+    await message.answer(emoji.emojize(
+        f"Hello:wave: I will help you to calculate your scholarship:moneybag:\n"
+        "Just send me your grades in a row. Example: AABBCP or A2B2CP\n"
+        "\n"
+        "Programmer: @azamatbayramov", language="alias"))
+
+
+async def get_emoji(scholarship: int) -> str:
+    return emoji.emojize(SCHOLARSHIP_EMOJI[scholarship // 1000], language="alias")
 
 
 @dp.message_handler()
@@ -49,10 +82,12 @@ async def handle_grades(message: types.Message):
         grades = await parse_grades(message.text)
         gpa = await get_gpa(grades)
         scholarship = await calculate_scholarship(gpa)
+        emoji_str = await get_emoji(scholarship)
         rounded_scholarship = round(scholarship) // 100 * 100
-        await message.answer(f"Your scholarship is {rounded_scholarship} rubles.")
+        await message.answer(f"{emoji_str} Your scholarship will be {rounded_scholarship} rubles {emoji_str}")
     except Exception:
-        await message.answer("Something went wrong. Try again.")
+        await message.answer(emoji.emojize("Something went wrong, try again :cry:\n\n"
+                                           "If you need help, write me /help :wink:", language="alias"))
 
 
 async def main():
