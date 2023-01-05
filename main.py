90cdb786fbd053f3914dc265aaa9bef24b2da225
Azamat Bayramov <bayramov.azamat04@gmail.com>
2023-01-05 19:49:44 +0500

    Add json using, refactor code

--- main.py
+++ main.py
@@ -1,52 +1,71 @@
 import asyncio
 import os
-
+import json
 import emoji
 from aiogram import Bot, Dispatcher, types
 from dotenv import load_dotenv
 
 load_dotenv()
 
-GRADES = {
-    "a": 5,
-    "b": 4,
-    "c": 3,
-    "d": 2,
-    "p": 5,
-    "f": 2
-}
+with open("grades_values.json") as file:
+    GRADES_VALUES = json.load(file)
 
-SCHOLARSHIP_EMOJI = {
-    3: ":dizzy_face:",
-    4: ":astonished:",
-    5: ":confounded:",
-    6: ":sob:",
-    7: ":cold_sweat:",
-    8: ":fearful:",
-    9: ":cry:",
-    10: ":pensive:",
-    11: ":unamused:",
-    12: ":grimacing:",
-    13: ":confused:",
-    14: ":sweat_smile:",
-    15: ":relieved:",
-    16: ":relaxed:",
-    17: ":grinning:",
-    18: ":smirk:",
-    19: ":blush:",
-    20: ":heart_eyes:"
-}
+with open("scholarship_emojis.json") as file:
+    SCHOLARSHIP_EMOJIS = json.load(file)
 
-GPA_EMOJI = {
-    2: ":dizzy_face:",
-    3: ":pensive:",
-    4: ":relaxed:",
-    5: ":heart_eyes:"
-}
+with open("gpa_emojis.json") as file:
+    GPA_EMOJIS = json.load(file)
 
-B_min = 3000
-B_max = 20000
+with open("settings.json") as file:
+    SETTINGS = json.load(file)
+
+with open("message_texts.json") as file:
+    MESSAGE_TEXTS = json.load(file)
+
+B_MIN = SETTINGS["B_MIN"]
+B_MAX = SETTINGS["B_MAX"]
 TOKEN = os.environ.get("TOKEN")
+
+HELP_MESSAGE = "help"
+SCHOLARSHIP_MESSAGE = "scholarship_message"
+SOMETHING_WENT_WRONG = "something_went_wrong"
+
+
+async def convert_grade_to_value(grade: str) -> int:
+    return GRADES_VALUES[grade]
+
+
+async def get_emoji_alias_by_gpa(gpa: float) -> str:
+    return GPA_EMOJIS[str(round(gpa))]
+
+
+async def get_emoji_alias_by_scholarship(scholarship: int) -> str:
+    return SCHOLARSHIP_EMOJIS[str(scholarship // 1000)]
+
+
+async def get_message(message_type, incoming_message_text=None):
+    message_with_emoji_aliases: str
+
+    if message_type == SCHOLARSHIP_MESSAGE:
+        incoming_message_text = incoming_message_text.lower()
+
+        grades = await parse_grades(incoming_message_text)
+
+        gpa = await calculate_gpa(grades)
+        gpa_emoji_alias = await get_emoji_alias_by_gpa(gpa)
+
+        scholarship = await calculate_scholarship(gpa)
+        scholarship_alias = await get_emoji_alias_by_scholarship(scholarship)
+
+        message_template: str = MESSAGE_TEXTS[message_type]
+
+        message_with_emoji_aliases = message_template.format(
+            round(gpa, 2), gpa_emoji_alias, scholarship, scholarship_alias
+        )
+    else:
+        message_with_emoji_aliases = MESSAGE_TEXTS[message_type]
+
+    return emoji.emojize(message_with_emoji_aliases, language="alias")
 
 
 async def parse_grades(grades_str: str) -> list[int]:
@@ -57,17 +76,17 @@ async def parse_grades(grades_str: str) -> list[int]:
             n = int(grade_str) - 1
             grades += [grades[-1]] * n
         else:
-            grades.append(GRADES[grade_str])
+            grades.append(await convert_grade_to_value(grade_str))
 
     return grades
 
 
-async def get_gpa(grades: list[int]) -> float:
+async def calculate_gpa(grades: list[int]) -> float:
     return sum(grades) / len(grades)
 
 
 async def calculate_scholarship(gpa: float) -> int:
-    return round(B_min + (B_max - B_min) * (((gpa - 2) / 3) ** 2.5)) // 100 * 100
+    return round(B_MIN + (B_MAX - B_MIN) * (((gpa - 2) / 3) ** 2.5)) // 100 * 100
 
 
 bot = Bot(token=TOKEN)
@@ -77,39 +96,23 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=["start", "help"])
 async def handle_help(message: types.Message):
-    await message.answer(emoji.emojize(
-        f"Hello:wave: I will help you to calculate your scholarship:moneybag:\n"
-        "Just send me your grades in a row. Example: AABBCP or A2B2CP\n"
-        "\n"
-        "Programmer: @azamatbayramov", language="alias"))
-
-
-async def get_emoji(scholarship: int) -> str:
-    return emoji.emojize(SCHOLARSHIP_EMOJI[scholarship // 1000], language="alias")
-
-
-async def get_emoji_by_gpa(gpa: float) -> str:
-    return emoji.emojize(GPA_EMOJI[int(gpa)], language="alias")
+    answer_message = await get_message(HELP_MESSAGE)
+    await message.answer(answer_message)
 
 
 @dp.message_handler(commands=["ping"])
-async def handle_help(message: types.Message):
+async def handle_ping(message: types.Message):
     await message.answer("pong")
 
 
 @dp.message_handler()
 async def handle_grades(message: types.Message):
     try:
-        grades = await parse_grades(message.text.lower())
-        gpa = await get_gpa(grades)
-        gpa_emoji = await get_emoji_by_gpa(gpa)
-        scholarship = await calculate_scholarship(gpa)
-        emoji_str = await get_emoji(scholarship)
-        await message.answer(
-            f"Your GPA will be {round(gpa, 2)}{gpa_emoji}\nYour scholarship will be {scholarship} rubles {emoji_str}")
-    except Exception:
-        await message.answer(emoji.emojize("Something went wrong, try again :cry:\n\n"
-                                           "If you need help, write me /help :wink:", language="alias"))
+        answer_message = await get_message(SCHOLARSHIP_MESSAGE, message.text)
+        await message.answer(answer_message)
+    except Exception as e:
+        answer_message = await get_message(SOMETHING_WENT_WRONG)
+        await message.answer(answer_message)
 
 
 async def main():
